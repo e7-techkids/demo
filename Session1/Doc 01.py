756e58b284dd564a13e1b8f7e69d828c53012d49
qhuydtvt <qhuydtvt@gmail.com>
2016-11-18 22:19:24 +0700

    Added code for session1

---
+++ Session1/Doc 01.py
@@ -0,0 +1,20 @@
+from turtle import *
+speed(-1)
+shape("turtle")
+for i in range(1,5,2):
+    forward(100)
+    lt(90)
+    forward(100)
+    lt(90)
+    forward(100)
+    lt(90)
+    forward(100)
+
+    left(7)
+    print(i)
+
+print("helloworld")
+
+
+
+
